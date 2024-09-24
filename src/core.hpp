684ef3fe1164ccfdf40a7cd83c9098b76d0821ad
Epsylene <epsylene@gmail.com>
2024-09-24 15:22:57 +0200

    Print errors and other messages with fmt

--- src/core.hpp
+++ src/core.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include "mglpch.hpp"
 
-#define MGL_ASSERT(x, msg) { if(!(x)) { printf(msg "\n"); assert(false); } }
+#include <fmt/core.h>
+
+#define MGL_ASSERT(x, msg, ...) { if(!(x)) { error(msg, ##__VA_ARGS__); assert(false); } }
 
 namespace minigl
 {
@@ -16,5 +18,20 @@ namespace minigl
     template<typename T, typename... Args>
     constexpr Box<T> box(Args&&... args) {
         return std::make_unique<T>(std::forward<Args>(args)...);
+    }
+
+    template<typename... Ts>
+    constexpr auto print(const std::string& str, Ts&&... args) {
+        return fmt::print(fmt::runtime(str), std::forward<Ts>(args)...);
+    }
+    
+    template<typename... Ts>
+    constexpr auto println(const std::string& str, Ts&&... args) {
+        return fmt::print(fmt::runtime(str + "\n"), std::forward<Ts>(args)...);
+    }
+
+    template<typename... Ts>
+    constexpr auto error(const std::string& str, Ts&&... args) {
+        return fmt::print(stderr, fmt::runtime(str + "\n"), std::forward<Ts>(args)...);
     }
 }
