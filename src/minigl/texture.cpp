fd7f34d146c0be1fa88beef6794dd2f054bfc3a7
Epsylene <epsylene@gmail.com>
2024-10-01 16:15:47 +0200

    Default wrap behavior for textures

--- src/minigl/texture.cpp
+++ src/minigl/texture.cpp
@@ -21,6 +21,12 @@ namespace minigl
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+        // Set the wrap mode to repeat (repeat the texture when
+        // the texture coordinates are outside the [0, 1]
+        // range).
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
         // Bind the texture to OpenGL.
         glBindTexture(GL_TEXTURE_2D, texture);
     }
