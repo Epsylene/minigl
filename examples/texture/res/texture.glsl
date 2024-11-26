750add6111168c09bcf7535a93f6922e9a215edd
Epsylene <epsylene@gmail.com>
2024-11-26 15:11:13 +0100

    Explicitly add the binding point to the texture sampler for better practices

--- examples/texture/res/texture.glsl
+++ examples/texture/res/texture.glsl
@@ -19,7 +19,7 @@ void main()
 #ifdef FRAGMENT
 layout(location = 0) out vec4 color;
 
-uniform sampler2D u_texture;
+layout(binding = 0) uniform sampler2D u_texture;
 
 in vec3 v_pos;
 in vec2 v_tex;
