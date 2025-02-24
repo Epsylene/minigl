9770a2830cfc086c3cc45cd423b308fcf758dee7
Epsylene <epsylene@gmail.com>
2025-02-24 11:07:03 +0100

    Correct prefix for input codes

--- src/minigl/app/input/mouse_codes.h
+++ src/minigl/app/input/mouse_codes.h
@@ -1,15 +1,14 @@
-
 #pragma once
 
-#define GLS_MOUSE_BUTTON_1         0
-#define GLS_MOUSE_BUTTON_2         1
-#define GLS_MOUSE_BUTTON_3         2
-#define GLS_MOUSE_BUTTON_4         3
-#define GLS_MOUSE_BUTTON_5         4
-#define GLS_MOUSE_BUTTON_6         5
-#define GLS_MOUSE_BUTTON_7         6
-#define GLS_MOUSE_BUTTON_8         7
-#define GLS_MOUSE_BUTTON_LAST      GLS_MOUSE_BUTTON_8
-#define GLS_MOUSE_BUTTON_LEFT      GLS_MOUSE_BUTTON_1
-#define GLS_MOUSE_BUTTON_RIGHT     GLS_MOUSE_BUTTON_2
-#define GLS_MOUSE_BUTTON_MIDDLE    GLS_MOUSE_BUTTON_3
+#define GLFW_MOUSE_BUTTON_1         0
+#define GLFW_MOUSE_BUTTON_2         1
+#define GLFW_MOUSE_BUTTON_3         2
+#define GLFW_MOUSE_BUTTON_4         3
+#define GLFW_MOUSE_BUTTON_5         4
+#define GLFW_MOUSE_BUTTON_6         5
+#define GLFW_MOUSE_BUTTON_7         6
+#define GLFW_MOUSE_BUTTON_8         7
+#define GLFW_MOUSE_BUTTON_LAST      GLFW_MOUSE_BUTTON_8
+#define GLFW_MOUSE_BUTTON_LEFT      GLFW_MOUSE_BUTTON_1
+#define GLFW_MOUSE_BUTTON_RIGHT     GLFW_MOUSE_BUTTON_2
+#define GLFW_MOUSE_BUTTON_MIDDLE    GLFW_MOUSE_BUTTON_3
